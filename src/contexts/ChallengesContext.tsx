e39043127c5f0b90d3c2ac4e0311a9b053bb478c
leandro <le@pop-os.localdomain>
2021-02-27 11:34:53 -0300

    tree commit 27/02

---
+++ src/contexts/ChallengesContext.tsx
@@ -0,0 +1,52 @@
+import { createContext, useState, ReactNode } from 'react';
+
+//dando tipos
+interface ChallengesContextData {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+  levelUp: () => void;
+  startNewChallenge: () => void;
+}
+
+interface ChallengesProvidesProps {
+  children: ReactNode;
+}
+
+export const ChallengesContext = createContext({} as ChallengesContextData);
+
+
+export function ChallengesProvider({ children }: ChallengesProvidesProps) {
+
+  const [level, setLevel] = useState(1);
+
+  const [currentExperience, setCurrentExperience] = useState(0);
+
+  const [challengesCompleted, setChallengesCompleted] = useState(0);
+
+
+  function levelUp() {
+    setLevel(level + 1);
+    setLevel(level)
+
+  }
+
+  function startNewChallenge() {
+    console.log(` novo challenge`);
+
+  }
+
+  return (
+    <ChallengesContext.Provider value={
+      level,
+      currentExperience,
+      challengesCompleted,
+      levelUp,
+      startNewChallenge
+
+    }
+    >
+      {children}
+    </ChallengesContext.Provider >
+  )
+}
